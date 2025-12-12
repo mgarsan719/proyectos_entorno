94b50db98647232e3cad4b5d01cb96387dfb436c
mgarsan719 <margagon154@gmail.com>
2025-12-12 12:14:37 +0100

    Update CuentaTest.java

--- eclipse/pruebasCodigo/src/pruebas/CuentaTest.java
+++ eclipse/pruebasCodigo/src/pruebas/CuentaTest.java
@@ -1,60 +1,52 @@
 package pruebas;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CuentaTest {
+	
+	private static Cuenta cuenta;
+	
+	@BeforeAll
+	public static void nuevaCuenta() {
+		cuenta = new Cuenta("ES567984135214657587", 100);
+	}
+	
 
-    private Cuenta cuenta;
+	@Test
+	void testGetSaldo() {
+		//Cuenta cuenta1= new Cuenta("ES567984135214657587", 100);
+		float saldo = cuenta.getSaldo();
+		assertEquals(100,saldo);
+	}
+	
+	@Test
+	void testSetSaldo() {
+		//Cuenta cuenta1= new Cuenta("ES567984135214657587", 100);
+		cuenta.setSaldo(100);
+		assertEquals(100,cuenta.getSaldo());
+	}
+	
+	@Test
+	void testIngresarDinero() {
+		cuenta.ingresarDinero(400);
+		assertEquals(500,cuenta.getSaldo());
+	}
 
-    @BeforeEach
-    public void nuevaCuenta() {
-
-        this.cuenta = new Cuenta("ES6475893745", 100);
-
-    }
-
-
-    @Test
-    void testGetSaldo() {
-        // Cuenta cuenta1 = new Cuenta("ES6475893745", 100);
-        double saldo = cuenta.getSaldo();
-        assertEquals(100, saldo);
-    }
-
-    @Test
-    void testSetSaldo() {
-        // Cuenta cuenta1 = new Cuenta("ES6475893745", 100);
-        cuenta.setSaldo(200);
-        assertEquals(200, 200);
-    }
-
-    @Test
-    void testIngresarDinero() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    void testExtraerDinero() {
-        try {
-
-            Cuenta cuenta1 = new Cuenta("ES6475893745", 100);
-            cuenta1.extraerDinero(90);
-            fail("ERROR. Se debería haber lanzado una excepción.");
-
-        }catch(ArithmeticException ae) {
-
-            //Prueba correcta
-
-        }
-    }
-
-    @Test
-    void testMostrarCuenta() {
-        fail("Not yet implemented");
-    }
-
+	@Test
+	void testExtraerDinero() {
+		try {
+			//Cuenta cuenta = new Cuenta("ES567984135214657587", 100);
+			cuenta.extraerDinero(120);
+			fail("ERROR. Se debería haber lanzado una excepción");
+		
+		}
+		catch(ArithmeticException ae) {
+			//Prueba correcta
+		}
+	}
 }
