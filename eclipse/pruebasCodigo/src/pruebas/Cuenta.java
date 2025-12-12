91e3921b20b83510860b4e8d41d42e59ce1f1de8
mgarsan719 <margagon154@gmail.com>
2025-12-12 12:11:10 +0100

    Update Cuenta.java

--- eclipse/pruebasCodigo/src/pruebas/Cuenta.java
+++ eclipse/pruebasCodigo/src/pruebas/Cuenta.java
@@ -1,56 +1,43 @@
 package pruebas;
 
-public class Cuenta
-{
-
-    //ATRIBUTOS
-
-    private String numero;
-    private double saldo;
-
-    public Cuenta(String numero, double saldo) {
-        this.setNumero(numero);
-        this.setSaldo(saldo);
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    public void ingresarDinero(double importe) {
-        setSaldo(getSaldo() + importe);
-    }
-
-    public void extraerDinero(double importe) {
-        if((saldo - importe)<0) {
-
-            throw new java.lang.ArithmeticException("Saldo negativo.");
-
-        }else {
-
-            saldo = saldo - importe;
-
-        }
-
-    }
-
-    public void mostrarCuenta() {
-
-        System.out.println("Nº Cuenta: "+getNumero());
-        System.out.println("Saldo: "+getSaldo()+ " €");
-
-    }
+public class Cuenta {
+	private String numero;
+	private float saldo;
 	
+	public Cuenta(String numCta, float saldoCta) {
+		setNumero(numCta);
+		setSaldo(saldoCta);
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void ingresarDinero(float importe) {
+		saldo= saldo+importe;
+	}
+	
+	public void extraerDinero(float importe) {
+		if((saldo-importe)<0)
+			throw new java.lang.ArithmeticException("Saldo negativo");
+		else
+			saldo = saldo - importe;
+	}
+	
+	public void mostrarCuenta() {
+		System.out.println("Nº Cuenta: "+getNumero());
+		System.out.println("Saldo: "+getSaldo()+ " €");
+	}
 }
